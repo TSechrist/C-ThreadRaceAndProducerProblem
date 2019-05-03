e58ff8ac748909a9f34c628c9ba95b1f3f90ee3d
Thomas Sechrist <tsechrist@mail.sfsu.edu>
2019-05-03 06:58:01 -0700

    changing to desktop

--- part2/pandc.c
+++ part2/pandc.c
@@ -8,8 +8,13 @@
 #include <semaphore.h>
 
 pthread_mutex_t lock;
-_Atomic int global = 0;
+sem_t empty;
+_Atomic int globalP = 0;
+_Atomic int globalC = 0;
+int n, x, p, c, ptime, consumetime, y;
 
+struct timespec tsP = {};
+struct timespec tsC = {};
 /* 
  * Function to remove item.
  * Item removed is returned
@@ -19,7 +24,7 @@ int dequeue_item()
     
 }
 
-/* 
+/*
  * Function to add item.
  * Item added is returned.
  * It is up to you to determine
@@ -35,18 +40,45 @@ int enqueue_item(int item)
 
 void* thread_func_producer( void* arg ){
 
-    pthread_mutex_lock(&lock);
+    for(int i = 0; i < x; i++)
+    {
 
-    pthread_mutex_unlock(&lock);
+        /* Aquire semaphore */
+        sem_wait(&empty);
+
+/* critical section */
+        globalP++;
+
+        printf("Producer: %d  ::  %d\n", (unsigned int)pthread_self(), globalP);
+
+/* release semaphore */
+        sem_post(&empty);
+
+        nanosleep(&tsP, 0);
+    }
 
     return NULL;
 }
 
 void* thread_func_consumer( void* arg ){
 
-    pthread_mutex_lock(&lock);
 
-    pthread_mutex_unlock(&lock);
+    for(int i = 0; i < y; i++)
+    {
+
+        /* Aquire semaphore */
+        sem_wait(&empty);
+
+/* critical section */
+        globalC++;
+
+        printf("Consumer: %d  ::  %d\n", (unsigned int)pthread_self(), globalC);
+
+/* release semaphore */
+        sem_post(&empty);
+
+        nanosleep(&tsC, 0);
+    }
 
     return NULL;
 }
@@ -58,14 +90,19 @@ int main(int argc, char** argv) {
         perror("Not correct number of arguments given (7)");
         exit( EXIT_FAILURE );
     }
+/* Create semaphore and initialize to 1 */
+    sem_init(&empty, 0, 1);
 
-    int n = atoi(argv[1]);
-    int p = atoi(argv[2]);
-    int c = atoi(argv[3]);
-    int x = atoi(argv[4]);
-    int y = x*p/c;
-    int ptime = atoi(argv[5]);
-    int ctime = atoi(argv[6]);
+    n = atoi(argv[1]);
+    p = atoi(argv[2]);
+    c = atoi(argv[3]);
+    x = atoi(argv[4]);
+    y = x*p/c;
+    ptime = atoi(argv[5]);
+    consumetime = atoi(argv[6]);
+
+    struct timespec tsP = {ptime, 0};
+    struct timespec tsC = {consumetime, 0};
 
     printf("Number of Buffers :                             %d\n", n);
     printf("Number of Producers :                           %d\n", p);
@@ -80,12 +117,39 @@ int main(int argc, char** argv) {
     }
     else
     {
-        printf("Over Consume on? :                               0\n");
+        printf("Over Consume on? :                              0\n");
     }
 
     printf("Time each Producer Sleeps (seconds) :           %d\n", ptime);
-    printf("Time each Consumer Sleeps (seconds) :           %d\n", ctime);
+    printf("Time each Consumer Sleeps (seconds) :           %d\n", consumetime);
 
 
+    pthread_t ids[p + c];
 
+    for(int i = 0 ; i < p ; i++)
+    {
+        int idx = i;
+        pthread_create(&ids[i],
+                       NULL,
+                       thread_func_producer,
+                       (void *)&idx);
+    }
+
+    for(int i = 0 ; i < c ; i++)
+    {
+        int idx = i;
+        pthread_create(&ids[i + p],
+                       NULL,
+                       thread_func_consumer,
+                       (void *)&idx);
+    }
+
+    for(int i = 0; i < p + c; i++)
+    {
+        pthread_join(ids[i],NULL);
+    }
+
+    pthread_mutex_destroy(&lock);
+
+    return 0;
 }
